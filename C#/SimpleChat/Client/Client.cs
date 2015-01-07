ed36344657025aa5dc1fc4f184d150a8a4e51dac
tergabri <terminassian.gabriel@gmail.com>
2015-01-07 13:17:12 +0100

    https://www.youtube.com/watch?v=mNGeY0OVZ8A

---
+++ C#/SimpleChat/Client/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Socket sk = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1994);
+            sk.Connect(endPoint);
+
+            Console.Write("Enter message: ");
+            string msg = Console.ReadLine();
+            byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+            sk.Send(msgBuffer, 0, msgBuffer.Length, 0);
+
+            byte[] buffer = new byte[255];
+            int rec = sk.Receive(buffer, 0, buffer.Length, 0);
+
+            Array.Resize(ref buffer, rec);
+
+            Console.WriteLine("Received: {0} ", Encoding.Default.GetString(buffer));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/SimpleChat/SimpleChat/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleChat
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Socket listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listeningSocket.Bind(new IPEndPoint(0, 1994));
+            listeningSocket.Listen(0);//listen to local connections
+
+            Socket acc = listeningSocket.Accept();//creates a new connection with the same IP/ports than the initial on
+
+            byte[] accBuffer = Encoding.Default.GetBytes("Hello client, I just saw your connection :-)");
+            acc.Send(accBuffer, 0, accBuffer.Length,0);
+
+            accBuffer = new byte[255];
+
+            int rec = acc.Receive(accBuffer, 0, accBuffer.Length, 0);
+
+            Array.Resize(ref accBuffer, rec);
+
+            Console.WriteLine("Received {0}", Encoding.Default.GetString(accBuffer));
+
+            listeningSocket.Close();
+            acc.Close();
+
+            Console.Read();
+
+
+        }
+    }
+}
